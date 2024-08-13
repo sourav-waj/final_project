2bf5289e633f092e957eb184e02e972b9a123365
sourav-waj <145178570+sourav-waj@users.noreply.github.com>
2024-08-13 07:30:39 -0600

    added load more shop section feature

--- index.js
+++ index.js
@@ -8,3 +8,22 @@ accordians.forEach(accordian => {
         accordian.classList.toggle("active")
     })
 })
+
+
+//Load more section for shopping content section
+
+let loadMoreButton = document.querySelector('#load');
+let currentItem = 3;
+
+loadMoreButton.onclick = () => {
+    let boxes = [...document.querySelectorAll('.card')];
+    for (var i = currentItem; i < currentItem + 3 && i < boxes.length; i++) {
+        boxes[i].style.display = 'inline-block';
+    }
+    currentItem += 3;
+
+
+    if (currentItem >= boxes.length){
+        loadMoreButton.style.display = 'none'
+    }
+}
